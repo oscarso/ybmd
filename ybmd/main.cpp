bebde66a979fef23b2246eaa494e0634f9377213
OscarSoDell7710\Oscar So <oscarso>
2016-09-09 15:37:08 -0700

    Get ride of mhook. Displaying more debug info for CardAcquireContext

--- ybmd/main.cpp
+++ ybmd/main.cpp
@@ -1,32 +1,14 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <mutex>
-#include "../mhook/mhook-lib/mhook.h"
 #include "../clogger/logger.h"
 #include "../inc_cpdk/cardmod.h"
 
 
 // Global Variables
 #define				LOG_PATH		"C:\\Logs\\"
-#if 0
-#define				APP_HOOKING_W	L"C:\\Yubico\\open_src_my\\SCMiniDriverTest\\x64\\Debug\\SCMiniDriverTest.exe"
-#else
-#define				APP_HOOKING_W	L"C:\\Windows\\system32\\LogonUI.exe"
-#endif
-#if 0
-#define				DLL_HOOKED_W	L"opensc-minidriver.dll"
-#define				DLL_HOOKED		"opensc-minidriver.dll"
-#else
-#define				DLL_HOOKED_W	L"msclmd.dll"
-#define				DLL_HOOKED		"msclmd.dll"
-#endif
 LOGGER::CLogger*	logger = NULL;
 HMODULE				g_hDll = 0;
-
-
-//initialization of MS Class Mini-driver API function pointers
-PFN_CARD_ACQUIRE_CONTEXT		pOrigCardAcquireContext = NULL;
-PFN_CARD_CHANGE_AUTHENTICATOR	pOrigCardChangeAuthenticator = NULL;
 
 
 //CardAcquireContext
@@ -36,14 +18,69 @@ CardAcquireContext(
 	__in	DWORD		dwFlags
 )
 {
-	DWORD	dwRet;
+	DWORD	dwRet = NO_ERROR;
 	if (logger) {
 		logger->TraceInfo("CardAcquireContext");
 		logger->TraceInfo("IN dwFlags: %x", dwFlags);
+		logger->TraceInfo("IN pCardData->dwVersion: %d", pCardData->dwVersion);
+		logger->TraceInfo("IN pCardData->pbAtr:");
+		logger->PrintBuffer(pCardData->pbAtr, pCardData->cbAtr);
+		logger->TraceInfo("IN pCardData->pwszCardName:");
+		logger->PrintBuffer(pCardData->pwszCardName, lstrlen(pCardData->pwszCardName));
+		logger->TraceInfo("IN pCardData->pfnCspAlloc: %p", &(pCardData->pfnCspAlloc));
+		logger->TraceInfo("IN pCardData->pfnCspReAlloc: %p", &(pCardData->pfnCspReAlloc));
+		logger->TraceInfo("IN pCardData->pfnCspFree: %p", &(pCardData->pfnCspFree));
+		logger->TraceInfo("IN pCardData->pfnCspCacheAddFile: %p", &(pCardData->pfnCspCacheAddFile));
+		logger->TraceInfo("IN pCardData->pfnCspCacheLookupFile: %p", &(pCardData->pfnCspCacheLookupFile));
+		logger->TraceInfo("IN pCardData->pfnCspCacheDeleteFile: %p", &(pCardData->pfnCspCacheDeleteFile));
+		logger->TraceInfo("IN pCardData->pvCacheContext: %x", pCardData->pvCacheContext);
+		logger->TraceInfo("IN pCardData->pfnCspPadData: %p", &(pCardData->pfnCspPadData));
+		logger->TraceInfo("IN pCardData->hSCardCtx: %x", pCardData->hSCardCtx);
+		logger->TraceInfo("IN pCardData->hScard: %x", pCardData->hScard);
 	}
-	dwRet = pOrigCardAcquireContext(pCardData, dwFlags);
-	pCardData->pfnCardChangeAuthenticator = CardChangeAuthenticator;
+
+	//pCardData->pfnCardDeleteContext = CardDeleteContext;
+	//pCardData->pfnCardAuthenticatePin = CardAuthenticatePin;
+	//pCardData->pfnCardChangeAuthenticator = CardChangeAuthenticator;
+
 	return dwRet;
+}
+
+
+#if 0
+//CardDeleteContext
+DWORD WINAPI
+CardDeleteContext(
+	__inout		PCARD_DATA	pCardData
+)
+{
+	if (logger) {
+		logger->TraceInfo("CardDeleteContext");
+	}
+	return pOrigCardDeleteContext(pCardData);
+}
+
+
+//CardAuthenticatePin
+DWORD WINAPI
+CardAuthenticatePin(
+	__in					PCARD_DATA	pCardData,
+	__in					LPWSTR		pwszUserId,
+	__in_bcount(cbPin)		PBYTE		pbPin,
+	__in					DWORD		cbPin,
+	__out_opt				PDWORD		pcAttemptsRemaining
+)
+{
+	if (logger) {
+		logger->TraceInfo("CardAuthenticatePin");
+	}
+	return pOrigCardAuthenticatePin(
+				pCardData,
+				pwszUserId,
+				pbPin,
+				cbPin,
+				pcAttemptsRemaining
+				);
 }
 
 
@@ -76,6 +113,7 @@ CardChangeAuthenticator(
 				pcAttemptsRemaining
 				);
 }
+#endif
 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -83,44 +121,6 @@ CardChangeAuthenticator(
 //	Private Helper Functions
 //
 //////////////////////////////////////////////////////////////////////////////////////
-
-//shouldHook
-bool shouldHook() {
-	wchar_t	wProcessName[MAX_PATH];
-	GetModuleFileName(NULL, wProcessName, MAX_PATH);
-	std::wstring wsPN(wProcessName);//convert wchar* to wstring
-	std::string strProcessName(wsPN.begin(), wsPN.end());
-	if (0 == wcscmp(APP_HOOKING_W, wProcessName)) {
-		logger = LOGGER::CLogger::getInstance(LOGGER::LogLevel_Info, LOG_PATH, "");
-		if (logger) { logger->TraceInfo("%s is calling %s", strProcessName.c_str(), DLL_HOOKED); }
-		return true;
-	}
-	return false;
-}
-
-
-//hookInitialize
-void hookInitialize() {
-	g_hDll = LoadLibrary(DLL_HOOKED_W);
-
-	//GetProcAddress
-	pOrigCardAcquireContext = (PFN_CARD_ACQUIRE_CONTEXT)GetProcAddress(g_hDll, "CardAcquireContext");
-	pOrigCardChangeAuthenticator = (PFN_CARD_CHANGE_AUTHENTICATOR)GetProcAddress(g_hDll, "CardChangeAuthenticator");
-
-	//Mhook_SetHook
-	//Mhook_SetHook((PVOID*)&pOrigCardAcquireContext, pHookCardAcquireContext);
-}
-
-
-//hookFinalize
-void hookFinalize() {
-	//Mhook_Unhook
-	//Mhook_Unhook((PVOID*)&pOrigCardAcquireContext);
-
-	pOrigCardAcquireContext = NULL;
-	pOrigCardChangeAuthenticator = NULL;
-}
-
 
 //DllMain
 BOOL WINAPI DllMain(
@@ -131,15 +131,9 @@ BOOL WINAPI DllMain(
 {
 	switch (Reason) {
 	case DLL_PROCESS_ATTACH:
-		if (shouldHook()) {
-			hookInitialize();
-		} else {
-			return FALSE;
-		}
 		break;
 
 	case DLL_PROCESS_DETACH:
-		hookFinalize();
 		break;
 	}
 	return TRUE;
