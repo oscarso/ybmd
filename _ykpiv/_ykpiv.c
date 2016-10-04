9a04cd28485ccf8bdc99c424bcdeeabab9be5ebc
OscarSoDell7710\Oscar So <oscarso>
2016-10-04 11:58:21 -0700

    Moved un-needed code into _ykpiv.c

---
+++ _ykpiv/_ykpiv.c
@@ -0,0 +1,160 @@
+/*
+
+	Keeping these code for testing and experimental purpose only.
+
+*/
+#if 0
+#define CHREF_ACT_CHANGE_PIN 0
+#define CHREF_ACT_UNBLOCK_PIN 1
+#define CHREF_ACT_CHANGE_PUK 2
+
+ykpiv_rc _transfer_data(ykpiv_state *state, const unsigned char *templ,
+	const unsigned char *in_data, long in_len,
+	unsigned char *out_data, unsigned long *out_len, int *sw) {
+	const unsigned char *in_ptr = in_data;
+	unsigned long max_out = *out_len;
+	ykpiv_rc res;
+	long rc;
+	*out_len = 0;
+
+	logger->TraceInfo("_transfer_data");
+
+	rc = SCardBeginTransaction(state->card);
+	if (rc != SCARD_S_SUCCESS) {
+		logger->TraceInfo("_transfer_data: SCardBeginTransaction rc=%x", rc);
+		return YKPIV_PCSC_ERROR;
+	}
+
+	do {
+		size_t this_size = 0xff;
+		unsigned char data[261];
+		unsigned long recv_len = sizeof(data);
+		APDU apdu;
+
+		memset(apdu.raw, 0, sizeof(apdu.raw));
+		memcpy(apdu.raw, templ, 4);
+		if (in_ptr + 0xff < in_data + in_len) {
+			apdu.st.cla = 0x10;
+		}
+		else {
+			this_size = (size_t)((in_data + in_len) - in_ptr);
+		}
+		if (1) {
+			logger->TraceInfo("Going to send %lu bytes in this go.\n", (unsigned long)this_size);
+		}
+		apdu.st.lc = (unsigned char)this_size;
+		memcpy(apdu.st.data, in_ptr, this_size);
+		res = _send_data(state, &apdu, data, &recv_len, sw);
+		logger->TraceInfo("1st _send_data: res=%d", res);
+		if (res != YKPIV_OK) {
+			return res;
+		}
+		else if (*sw != SW_SUCCESS && *sw >> 8 != 0x61) {
+			return YKPIV_OK;
+		}
+		if (*out_len + recv_len - 2 > max_out) {
+			if (1) {
+				logger->TraceInfo("Output buffer to small, wanted to write %lu, max was %lu.\n", *out_len + recv_len - 2, max_out);
+			}
+			return YKPIV_SIZE_ERROR;
+		}
+		if (out_data) {
+			memcpy(out_data, data, recv_len - 2);
+			out_data += recv_len - 2;
+			*out_len += recv_len - 2;
+		}
+		in_ptr += this_size;
+	} while (in_ptr < in_data + in_len);
+	while (*sw >> 8 == 0x61) {
+		APDU apdu;
+		unsigned char data[261];
+		unsigned long recv_len = sizeof(data);
+
+		if (1) {
+			logger->TraceInfo("The card indicates there is %d bytes more data for us.\n", *sw & 0xff);
+		}
+
+		memset(apdu.raw, 0, sizeof(apdu.raw));
+		apdu.st.ins = 0xc0;
+		res = _send_data(state, &apdu, data, &recv_len, sw);
+		logger->TraceInfo("2nd _send_data: res=%d", res);
+		if (res != YKPIV_OK) {
+			return res;
+		}
+		else if (*sw != SW_SUCCESS && *sw >> 8 != 0x61) {
+			return YKPIV_OK;
+		}
+		if (*out_len + recv_len - 2 > max_out) {
+			logger->TraceInfo("Output buffer to small, wanted to write %lu, max was %lu.", *out_len + recv_len - 2, max_out);
+		}
+		if (out_data) {
+			memcpy(out_data, data, recv_len - 2);
+			out_data += recv_len - 2;
+			*out_len += recv_len - 2;
+		}
+	}
+
+	rc = SCardEndTransaction(state->card, SCARD_LEAVE_CARD);
+	if (rc != SCARD_S_SUCCESS) {
+		logger->TraceInfo("_transfer_data: error: Failed to end pcsc transaction, rc=%08lx\n", rc);
+		return YKPIV_PCSC_ERROR;
+	}
+	return YKPIV_OK;
+}
+
+static ykpiv_rc _change_pin_internal(ykpiv_state *state, int action, const char * current_pin, size_t current_pin_len, const char * new_pin, size_t new_pin_len, int *tries) {
+	int sw;
+	unsigned char templ[] = { 0, YKPIV_INS_CHANGE_REFERENCE, 0, 0x80 };
+	unsigned char indata[0x10];
+	unsigned char data[0xff];
+	unsigned long recv_len = sizeof(data);
+	ykpiv_rc res;
+
+	logger->TraceInfo("_change_pin_internal");
+
+	if (current_pin_len > 8) {
+		return YKPIV_SIZE_ERROR;
+	}
+	if (new_pin_len > 8) {
+		return YKPIV_SIZE_ERROR;
+	}
+	if (action == CHREF_ACT_UNBLOCK_PIN) {
+		templ[1] = YKPIV_INS_RESET_RETRY;
+	}
+	else if (action == CHREF_ACT_CHANGE_PUK) {
+		templ[3] = 0x81;
+	}
+	memcpy(indata, current_pin, current_pin_len);
+	if (current_pin_len < 8) {
+		memset(indata + current_pin_len, 0xff, 8 - current_pin_len);
+	}
+	memcpy(indata + 8, new_pin, new_pin_len);
+	if (new_pin_len < 8) {
+		memset(indata + 8 + new_pin_len, 0xff, 8 - new_pin_len);
+	}
+	res = _transfer_data(state, templ, indata, sizeof(indata), data, &recv_len, &sw);
+	if (res != YKPIV_OK) {
+		return res;
+	}
+	else if (sw != SW_SUCCESS) {
+		if ((sw >> 8) == 0x63) {
+			*tries = sw & 0xf;
+			return YKPIV_WRONG_PIN;
+		}
+		else if (sw == SW_ERR_AUTH_BLOCKED) {
+			return YKPIV_PIN_LOCKED;
+		}
+		else {
+			if (1) {
+				logger->TraceInfo("Failed changing pin, token response code: %x.\n", sw);
+			}
+			return YKPIV_GENERIC_ERROR;
+		}
+	}
+	return YKPIV_OK;
+}
+
+ykpiv_rc _change_pin(ykpiv_state *state, const char * current_pin, size_t current_pin_len, const char * new_pin, size_t new_pin_len, int *tries) {
+	return _change_pin_internal(state, CHREF_ACT_CHANGE_PIN, current_pin, current_pin_len, new_pin, new_pin_len, tries);
+}
+#endif
--- ybmd/main.cpp
+++ ybmd/main.cpp
@@ -69,29 +69,24 @@ DWORD	ykrc2mdrc(const ykpiv_rc ykrc) {
 }
 
 
-#define CHREF_ACT_CHANGE_PIN 0
-#define CHREF_ACT_UNBLOCK_PIN 1
-#define CHREF_ACT_CHANGE_PUK 2
-
 static ykpiv_rc _send_data(ykpiv_state *state, APDU *apdu,
 	unsigned char *data, unsigned long *recv_len, int *sw) {
 	long rc;
 	unsigned int send_len = (unsigned int)apdu->st.lc + 5;
 
-	logger->TraceInfo("_send_data");
-	if (1) {
+	if (logger) {
+		logger->TraceInfo("_send_data");
 		logger->TraceInfo("Data Sent:");
 		logger->PrintBuffer(apdu->raw, send_len);
 	}
+
 	rc = SCardTransmit(state->card, SCARD_PCI_T1, apdu->raw, send_len, NULL, data, recv_len);
 	if (rc != SCARD_S_SUCCESS) {
-		if (1) {
-			logger->TraceInfo("error: SCardTransmit failed, rc=%08lx\n", rc);
-		}
+		if (logger) { logger->TraceInfo("error: SCardTransmit failed, rc=%08lx\n", rc); }
 		return YKPIV_PCSC_ERROR;
 	}
 
-	if (1) {
+	if (logger) {
 		logger->TraceInfo("Data Received:");
 		logger->PrintBuffer(data, *recv_len);
 	}
@@ -101,152 +96,6 @@ static ykpiv_rc _send_data(ykpiv_state *state, APDU *apdu,
 		*sw = 0;
 	}
 	return YKPIV_OK;
-}
-
-ykpiv_rc _transfer_data(ykpiv_state *state, const unsigned char *templ,
-	const unsigned char *in_data, long in_len,
-	unsigned char *out_data, unsigned long *out_len, int *sw) {
-	const unsigned char *in_ptr = in_data;
-	unsigned long max_out = *out_len;
-	ykpiv_rc res;
-	long rc;
-	*out_len = 0;
-
-	logger->TraceInfo("_transfer_data");
-
-	rc = SCardBeginTransaction(state->card);
-	if (rc != SCARD_S_SUCCESS) {
-		logger->TraceInfo("_transfer_data: SCardBeginTransaction rc=%x", rc);
-		return YKPIV_PCSC_ERROR;
-	}
-
-	do {
-		size_t this_size = 0xff;
-		unsigned char data[261];
-		unsigned long recv_len = sizeof(data);
-		APDU apdu;
-
-		memset(apdu.raw, 0, sizeof(apdu.raw));
-		memcpy(apdu.raw, templ, 4);
-		if (in_ptr + 0xff < in_data + in_len) {
-			apdu.st.cla = 0x10;
-		} else {
-			this_size = (size_t)((in_data + in_len) - in_ptr);
-		}
-		if (1) {
-			logger->TraceInfo("Going to send %lu bytes in this go.\n", (unsigned long)this_size);
-		}
-		apdu.st.lc = (unsigned char)this_size;
-		memcpy(apdu.st.data, in_ptr, this_size);
-		res = _send_data(state, &apdu, data, &recv_len, sw);
-		logger->TraceInfo("1st _send_data: res=%d", res);
-		if (res != YKPIV_OK) {
-			return res;
-		}
-		else if (*sw != SW_SUCCESS && *sw >> 8 != 0x61) {
-			return YKPIV_OK;
-		}
-		if (*out_len + recv_len - 2 > max_out) {
-			if (1) {
-				logger->TraceInfo("Output buffer to small, wanted to write %lu, max was %lu.\n", *out_len + recv_len - 2, max_out);
-			}
-			return YKPIV_SIZE_ERROR;
-		}
-		if (out_data) {
-			memcpy(out_data, data, recv_len - 2);
-			out_data += recv_len - 2;
-			*out_len += recv_len - 2;
-		}
-		in_ptr += this_size;
-	} while (in_ptr < in_data + in_len);
-	while (*sw >> 8 == 0x61) {
-		APDU apdu;
-		unsigned char data[261];
-		unsigned long recv_len = sizeof(data);
-
-		if (1) { 
-			logger->TraceInfo("The card indicates there is %d bytes more data for us.\n", *sw & 0xff);
-		}
-
-		memset(apdu.raw, 0, sizeof(apdu.raw));
-		apdu.st.ins = 0xc0;
-		res = _send_data(state, &apdu, data, &recv_len, sw);
-		logger->TraceInfo("2nd _send_data: res=%d", res);
-		if (res != YKPIV_OK) {
-			return res;
-		}
-		else if (*sw != SW_SUCCESS && *sw >> 8 != 0x61) {
-			return YKPIV_OK;
-		}
-		if (*out_len + recv_len - 2 > max_out) {
-			logger->TraceInfo("Output buffer to small, wanted to write %lu, max was %lu.", *out_len + recv_len - 2, max_out);
-		}
-		if (out_data) {
-			memcpy(out_data, data, recv_len - 2);
-			out_data += recv_len - 2;
-			*out_len += recv_len - 2;
-		}
-	}
-
-	rc = SCardEndTransaction(state->card, SCARD_LEAVE_CARD);
-	if (rc != SCARD_S_SUCCESS) {
-		logger->TraceInfo("_transfer_data: error: Failed to end pcsc transaction, rc=%08lx\n", rc);
-		return YKPIV_PCSC_ERROR;
-	}
-	return YKPIV_OK;
-}
-
-static ykpiv_rc _change_pin_internal(ykpiv_state *state, int action, const char * current_pin, size_t current_pin_len, const char * new_pin, size_t new_pin_len, int *tries) {
-	int sw;
-	unsigned char templ[] = { 0, YKPIV_INS_CHANGE_REFERENCE, 0, 0x80 };
-	unsigned char indata[0x10];
-	unsigned char data[0xff];
-	unsigned long recv_len = sizeof(data);
-	ykpiv_rc res;
-
-	logger->TraceInfo("_change_pin_internal");
-
-	if (current_pin_len > 8) {
-		return YKPIV_SIZE_ERROR;
-	}
-	if (new_pin_len > 8) {
-		return YKPIV_SIZE_ERROR;
-	}
-	if (action == CHREF_ACT_UNBLOCK_PIN) {
-		templ[1] = YKPIV_INS_RESET_RETRY;
-	} else if (action == CHREF_ACT_CHANGE_PUK) {
-		templ[3] = 0x81;
-	}
-	memcpy(indata, current_pin, current_pin_len);
-	if (current_pin_len < 8) {
-		memset(indata + current_pin_len, 0xff, 8 - current_pin_len);
-	}
-	memcpy(indata + 8, new_pin, new_pin_len);
-	if (new_pin_len < 8) {
-		memset(indata + 8 + new_pin_len, 0xff, 8 - new_pin_len);
-	}
-	res = _transfer_data(state, templ, indata, sizeof(indata), data, &recv_len, &sw);
-	if (res != YKPIV_OK) {
-		return res;
-	} else if (sw != SW_SUCCESS) {
-		if ((sw >> 8) == 0x63) {
-			*tries = sw & 0xf;
-			return YKPIV_WRONG_PIN;
-		}
-		else if (sw == SW_ERR_AUTH_BLOCKED) {
-			return YKPIV_PIN_LOCKED;
-		} else {
-			if (1) {
-				logger->TraceInfo("Failed changing pin, token response code: %x.\n", sw);
-			}
-			return YKPIV_GENERIC_ERROR;
-		}
-	}
-	return YKPIV_OK;
-}
-
-ykpiv_rc _change_pin(ykpiv_state *state, const char * current_pin, size_t current_pin_len, const char * new_pin, size_t new_pin_len, int *tries) {
-	return _change_pin_internal(state, CHREF_ACT_CHANGE_PIN, current_pin, current_pin_len, new_pin, new_pin_len, tries);
 }
 
 
@@ -638,13 +487,13 @@ CardChangeAuthenticator(
 	memcpy(oldpin, (const char *)pbCurrentAuthenticator, 8);
 	memcpy(newpin, (const char *)pbNewAuthenticator, 8);
 	logger->TraceInfo("CardChangeAuthenticator: oldpin=%s  newpin=%s", oldpin, newpin);
-	ykrc = _change_pin(
+	ykrc = ykpiv_change_pin(
 				&ykState,
 				(const char *)oldpin, 8,
 				(const char *)newpin, 8,
 				&tries);
 	if (logger) {
-		logger->TraceInfo("CardChangeAuthenticator: _change_pin: ykrc=%d; tries=%d", ykrc, tries);
+		logger->TraceInfo("CardChangeAuthenticator: ykpiv_change_pin: ykrc=%d; tries=%d", ykrc, tries);
 	}
 	return ykrc2mdrc(ykrc);
 } // of CardChangeAuthenticator
@@ -792,10 +641,28 @@ DWORD WINAPI CardWriteFile(
 	__in DWORD cbData
 )
 {
+	DWORD	dwRet = SCARD_S_SUCCESS;
 	if (logger) {
 		logger->TraceInfo("CardWriteFile");
+		logger->TraceInfo("IN pszDirectoryName: %s", pszDirectoryName);
+		logger->TraceInfo("IN pszFileName: %s", pszFileName);
+		logger->TraceInfo("IN dwFlags: %x", dwFlags);
+		logger->TraceInfo("IN pbData:");
+		logger->PrintBuffer(pbData, cbData);
 	}
-	if (!pCardData) return SCARD_E_INVALID_PARAMETER;
+	if (!pCardData)
+		return SCARD_E_INVALID_PARAMETER;
+	if (!pszFileName)
+		return SCARD_E_INVALID_PARAMETER;
+	if (!strlen(pszFileName))
+		return SCARD_E_INVALID_PARAMETER;
+	if (!pbData)
+		return SCARD_E_INVALID_PARAMETER;
+	if (0 == cbData)
+		return SCARD_E_INVALID_PARAMETER;
+	if (dwFlags)
+		return SCARD_E_INVALID_PARAMETER;
+
 	return SCARD_E_UNSUPPORTED_FEATURE;
 }
 
